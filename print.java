3e4fac162dc949a822fa1a26dad988ee8b619eca
dle04 <124306791+dle04@users.noreply.github.com>
2023-11-16 12:47:22 -0600

    Create print.java

---
+++ print.java
@@ -0,0 +1,15 @@
+public class print
+{
+public static void print()
+{
+int rows = 15;
+int columns = 15;
+	for (int i = 0; i < rows; i++) 
+	{
+			for (int j = 0; j < columns; j++) 
+			{
+                		System.out.println(Board[i][j]);
+            		}
+	}
+}
+}
